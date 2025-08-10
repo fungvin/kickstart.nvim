18401b6095dc1a641f1974ca63d6cbd3de75e381
vince <fung.vin@gmail.com>
2025-08-10 21:51:16 +0800

    feat: add logic to controll columns in a tab

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+-- Set tab width to 2 spaces
+vim.opt.tabstop = 2        -- Width of tab character
+vim.opt.softtabstop = 2    -- Fine tunes the amount of whitespace to be added
+vim.opt.shiftwidth = 2     -- Number of spaces to use for each step of (auto)indent
+vim.opt.expandtab = true   -- Use spaces instead of tabs
+
 return {}
