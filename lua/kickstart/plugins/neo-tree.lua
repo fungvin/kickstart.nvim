b4ddac2d087c33068c6eaa1f2a1dfb1f9bd94db0
vince <fung.vin@gmail.com>
2025-08-24 16:33:18 +0800

    refactor: enabled kickstart/plugins/neo-tree and merged opts with my existing one

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -20,6 +20,19 @@ return {
           ['\\'] = 'close_window',
         },
       },
+      filtered_items = {
+        visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        never_show = {
+          '.git',
+          '.idea',
+        },
+      },
+      follow_current_file = {
+        enabled = true, -- This will find and focus the file in the active buffer every time the current file is changed while the tree is open.
+        leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      },
     },
   },
 }
