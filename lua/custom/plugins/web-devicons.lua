bc040f7bf987915c959875cf93dc948128fa7b80
vince <fung.vin@gmail.com>
2025-09-01 01:47:00 +0800

    feat: add devicons to overide certain icons by extension

---
+++ lua/custom/plugins/web-devicons.lua
@@ -0,0 +1,22 @@
+return {
+  'nvim-tree/nvim-web-devicons', 
+  opts = {
+    override_by_extension = {
+      ['js'] = {
+        color  = '#F0DB4F',
+        icon = '',
+        name = 'JavaScript',
+      },
+      ['ts'] = {
+        color  = '#007ACC',
+        icon = '',
+        name = 'TypeScript',
+      },
+      ['d.ts'] = {
+        color  = '#007ACC',
+        icon = '',
+        name = 'TypeScript',
+      }
+    }
+  },
+}
