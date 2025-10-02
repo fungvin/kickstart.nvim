2170d9d410dedf755d84a73c3a7ccff56a107791
vince <fung.vin@gmail.com>
2025-10-02 17:40:46 +0700

    feat: add logic to call refresh in neotree when user exits lazygit

--- lua/custom/plugins/lazygit.lua
+++ lua/custom/plugins/lazygit.lua
@@ -8,13 +8,25 @@ return {
     'LazyGitFilter',
     'LazyGitFilterCurrentFile',
   },
-  -- optional for floating window border decoration
   dependencies = {
     'nvim-lua/plenary.nvim',
   },
-  -- setting the keybinding for LazyGit with 'keys' is recommended in
-  -- order to load the plugin when the command is run for the first time
   keys = {
     { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
   },
+  config = function()
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'lazygit',
+      callback = function()
+        vim.api.nvim_create_autocmd({ 'BufLeave' }, {
+          buffer = 0,
+          callback = function()
+            local events = require 'neo-tree.events'
+            events.fire_event(events.GIT_EVENT)
+          end,
+          once = true,
+        })
+      end,
+    })
+  end,
 }
