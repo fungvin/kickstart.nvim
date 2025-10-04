05c166564da1c18c3339a8e2bf998c82e755ab2b
vince <fung.vin@gmail.com>
2025-10-04 15:38:11 +0700

    feat: add folder for my own plugins, add floating terminal plugin

--- init.lua
+++ init.lua
@@ -985,6 +985,8 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   { import = 'custom.plugins' },
+  { import = 'custom.my-plugins' },
+
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
